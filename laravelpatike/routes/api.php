faef0f0a46ef49fb862f87c411b23a2a37ddc8a7
Milica Tomasevic <milicatomasevic000@gmail.com>
2023-08-26 00:36:33 +0200

    Kreirane api rute koje ce se pozivati

--- laravelpatike/routes/api.php
+++ laravelpatike/routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\BrendController;
+use App\Http\Controllers\BrendPatikaController;
+use App\Http\Controllers\KategorijaController;
+use App\Http\Controllers\PatikaController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,11 +15,35 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/users', [UserController::class,'index']);
+Route::get('/users/{id}', [UserController::class,'show']);
+
+
+Route::get('/kategorije', [KategorijaController::class,'index']);
+Route::get('/kategorija/{kategorija}', [KategorijaController::class,'show']);
+
+Route::get('/brendovi', [BrendController::class,'index']);
+Route::get('/brendovi/{brend}', [BrendController::class,'show']);
+
+Route::get('/patike', [PatikaController::class,'index']);
+Route::get('/patikeOdredjenogBrenda/{id}', [BrendPatikaController::class,'index']);
+
+Route::delete('/kategorija/{id}', [KategorijaController::class, 'destroy']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+    Route::put('patike/{id}', [PatikaController::class, 'update']);
+    Route::delete('patike/{id}', [PatikaController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
+
